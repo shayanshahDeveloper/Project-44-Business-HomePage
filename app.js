5fd35016b10f95df0bdceaf446818b56cdcaa36c
moviesworldbyshanie <127875416+moviesworldbyshanie@users.noreply.github.com>
2024-08-26 03:17:11 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function handleNavbar() {
+  document.getElementById("nav-phone").style.cssText = "top:0px";
+}
+
+function cutNavbar() {
+  document.getElementById("nav-phone").style.cssText = "top:-300px";
+}
